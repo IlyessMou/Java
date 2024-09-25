e0f5a0b62c5686da28f86639a51a9b6960726f38
unknown <ilyessmoulazem1@gmail.com>
2024-09-25 15:46:45 +0200

    Java_Fundamentals

---
+++ Java_Fundamentals/FirstJavaProject.java
@@ -0,0 +1,11 @@
+public class FirstJavaProject {
+    public static void main(String[] args) {
+        String name = "Ilyess";
+        int age = 21;
+        String hometown = "Bizerta";
+        
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
