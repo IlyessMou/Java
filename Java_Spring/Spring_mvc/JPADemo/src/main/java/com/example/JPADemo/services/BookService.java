76ed806e38420494818ca7935cdbac0b758fde8e
unknown <ilyessmoulazem1@gmail.com>
2024-11-05 15:36:23 +0100

    Java_Spring

---
+++ Java_Spring/Spring_mvc/JPADemo/src/main/java/com/example/JPADemo/services/BookService.java
@@ -0,0 +1,54 @@
+package com.example.JPADemo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.JPADemo.models.Book;
+import com.example.JPADemo.repositories.BookRepository;
+
+@Service
+public class BookService {
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    // Updates a book 
+    public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            Book bookToUpdate = optionalBook.get();
+            bookToUpdate.setTitle(title);
+            bookToUpdate.setDescription(desc);
+            bookToUpdate.setLanguage(lang);
+            bookToUpdate.setNumberOfPages(numOfPages);
+            return bookRepository.save(bookToUpdate);
+        }
+        return null; 
+    }
+
+    // Deletes book 
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
